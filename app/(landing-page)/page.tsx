de2e57dd9a0f27422e8780ec12a40d2a0abaa5f5
Abdul Rafay <152396110+abdulrafay-07@users.noreply.github.com>
2024-08-17 12:08:06 +0500

    starter setup

---
+++ app/(landing-page)/page.tsx
@@ -0,0 +1,14 @@
+const Home = () => {
+    return (
+      <div className='flex flex-col justify-center items-center h-full bg-neutral-900 text-gray-100 gap-2'>
+        <h1 className='text-7xl font-bold'>
+          tracker.io
+        </h1>
+        <p>
+          Coming soon...
+        </p>
+      </div>
+    )
+};
+
+export default Home;
